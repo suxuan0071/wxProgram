90f7fb72487b8ba5576687d2d450f7886b2280a8
zc <1418649355@qq.com>
2023-01-28 11:20:02 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,63 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+			<button type="default" @click="jumpClick">jump</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			
+		},
+		methods: {
+			jumpClick:function(){
+				uni.authorize({
+					scope:'scope.userInfo',
+					success: () => {
+						uni.alert('login success')
+					}
+				})
+				uni.navigateTo({
+					url:'../jumpHtml/jumpHtml',
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
